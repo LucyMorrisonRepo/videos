84c952d5a2a9ad6fca3b9fce65d08495083252f5
kofimorrison <49091928+kofimorrison@users.noreply.github.com>
2019-05-12 22:29:53 +0200

    Updates

---
+++ precache-manifest.3d065b405b945edb3e0979f10fcef38a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e96a756bb60825b728f1b15bb91903d9",
+    "url": "/videos/index.html"
+  },
+  {
+    "revision": "ab1e9bc3268b684130ee",
+    "url": "/videos/static/css/main.03c6ca28.chunk.css"
+  },
+  {
+    "revision": "7e6ac1444ef9242ebc00",
+    "url": "/videos/static/js/2.824d5390.chunk.js"
+  },
+  {
+    "revision": "ab1e9bc3268b684130ee",
+    "url": "/videos/static/js/main.3163c490.chunk.js"
+  },
+  {
+    "revision": "39e39b46196f171b2925",
+    "url": "/videos/static/js/runtime~main.52b05483.js"
+  }
+]);
